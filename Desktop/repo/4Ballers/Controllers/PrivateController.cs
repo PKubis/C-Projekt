24b9317c878aac1db20d99adfdae0419ed8f95c3
PKubis <piotrkubis1989@gmail.com>
2023-12-03 11:35:36 +0100

    druzyny

---
+++ Desktop/repo/4Ballers/Controllers/PrivateController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+[Authorize]
+public class PrivateController : Controller
+{
+    public IActionResult Shoes()
+    {
+        ViewData["Title"] = "Obuwie";
+        return View();
+    }
+
+    public IActionResult Basket()
+    {
+        ViewData["Title"] = "Koszyk";
+        return View();
+    }
+}
