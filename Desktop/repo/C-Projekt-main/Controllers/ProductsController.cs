5c017e478406701fd3d1203a07feee53d8770a32
PKubis <piotrkubis1989@gmail.com>
2023-12-12 06:12:34 +0100

    mail

---
+++ Desktop/repo/C-Projekt-main/Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _4Ballers.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
